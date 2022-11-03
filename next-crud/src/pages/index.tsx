097811dfd11bf6617a4c95a879d0a3622d538ef8
douglas.boeno <douglasrodriguesboeno@gmail.com>
2022-11-03 15:49:01 -0300

    Config projeto

---
+++ next-crud/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={`@tailwind
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-600
+    `}>
+      <span className='text-4xl'>Texto</span>
+    </div>
+  )
+}
